7e6a3178fede790a8976458ccb3816e9e105648b
mattf9 <nexus987@yahoo.com>
2020-12-07 21:12:11 -0500

    remove pictures for now, add seed/test data

--- db/migrate/20201205215637_create_marriages.rb
+++ db/migrate/20201205215637_create_marriages.rb
@@ -1,8 +1,8 @@
 class CreateMarriages < ActiveRecord::Migration[6.0]
   def change
     create_table :marriages do |t|
-      t.string :husband
-      t.string :wife
+      t.string :husband, null: false
+      t.string :wife, null: false
       t.string :start_year
       t.string :end_year
 
