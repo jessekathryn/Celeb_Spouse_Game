8e520cb21c8bebf8454dc52e67d3664b176c3bf1
mattf9 <nexus987@yahoo.com>
2020-12-24 19:04:47 -0500

    working quizzes, flash if correct or wrong

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'trivia#index'
+  post '/', to: 'trivia#guess'
   resources :marriages
   resources :celebrities
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
